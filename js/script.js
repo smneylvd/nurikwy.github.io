2d2a1d0a1398719a5074b9a30066f7e870b6c55f
Someone You loved <202082@astanait.edu.kz>
2021-12-14 16:47:24 +0600

    032

--- js/script.js
+++ js/script.js
@@ -45,6 +45,29 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  if (slides.length != 0) {
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
 }
+
+$(document).ready(function () {
+  function counter(id, start, end, duration) {
+  let obj = document.getElementById(id),
+   current = start,
+   range = end - start,
+   increment = end > start ? 1 : -1,
+   step = Math.abs(Math.floor(duration / range)),
+   timer = setInterval(() => {
+    current += increment;
+    obj.textContent = current;
+    if (current == end) {
+     clearInterval(timer);
+    }
+   }, step);
+ }
+ counter("count1", 0, 400, 3000);
+ counter("count2", 100, 50, 2500);
+ counter("count3", 0, 40, 3000);
+});
+
