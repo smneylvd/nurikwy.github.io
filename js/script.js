f5388eb4ad07739d99a99af5ca0c12b20dbec492
Someone You loved <202082@astanait.edu.kz>
2021-12-16 04:53:16 +0600

    +backend

--- js/script.js
+++ js/script.js
@@ -73,3 +73,143 @@ $(document).ready(function () {
  }
 });
 
+var persons = new Array();
+
+$(document).ready(function () {
+  json = window.localStorage.getItem('users');
+  if (json == null) {
+    let admin = {
+      email: "admin",
+      pass: "admin"
+    }
+    persons[0] = admin;
+    window.localStorage.setItem('users', JSON.stringify(persons))
+  }
+  else {
+    persons = JSON.parse(json);
+    console.log(persons);
+  }
+});
+
+
+$('button[name="signupbtn"]').on("click",function () {
+  let email = $('input[name="email"]').val();
+  let pass = $('input[name="pass"]').val();
+  let pass2 = $('input[name="pass-repeat"]').val();
+  if (pass != pass2) {
+    alert("Passwords does not match");
+  }
+  else {
+    let user = {
+      email: email,
+      pass: pass,
+      active: false
+    }
+    persons[persons.length] = user;
+    window.localStorage.setItem('users', JSON.stringify(persons));
+  }
+});
+
+$('button[name="signinbtn"]').on("click",function () {
+  let email = $('input[name="emaill"]').val();
+  let pass = $('input[name="passl"]').val();
+  for (var i = 0; i < persons.length; i++) {
+
+    if(persons[i]['email'] == email){
+
+      if (persons[i]['pass'] == pass) {
+
+        if (email == 'admin') {
+
+          window.localStorage.setItem('currentUser', email);
+          window.location.href = 'admin.html';
+          return;
+
+        }
+
+        if (persons[i]['active'] == false) {
+          alert("Your account is not activated!");
+          return;
+
+        }
+
+        window.localStorage.setItem('currentUser', email);
+        location.reload();
+        return;
+
+      }
+
+      else {
+
+        alert("Incorrect password");
+        return;
+
+      }
+    }
+  }
+  alert("Incorrect email");
+});
+
+$(document).ready(function () {
+  if (window.localStorage.getItem('currentUser') != null) {
+    $('.authorizeButton').hide();
+    $('.userSection').show();
+    $('.username').html(window.localStorage.getItem('currentUser'));
+  }
+  else {
+    $('.userSection').hide();
+    $('.authorizeButton').show();
+  }
+});
+
+$('#logOut').on("click",function () {
+  window.localStorage.removeItem('currentUser');
+  location.reload();  
+});
+
+if (document.location.href.match(/[^\/]+$/)[0] === 'admin.html') {
+  $(document).ready(function () {
+    for (var i = 1; i < persons.length; i++) {
+      let obj = ('<li class="users"><form><a href="#">User 1</a><p>Email: ' + persons[i]["email"]+ '</p><p>Active: ' + persons[i]["active"]+ '</p><button class="btn-red" value="' + persons[i]["email"]+ '" type="button"/>Disable</button><button class="btn-orange" value="' + persons[i]["email"]+ '" type="button"/>Enable</button><button class="btn-red" value="' + persons[i]["email"]+ '" type="button"/>Delete</button></form></li>');
+      $("#users").append(obj);
+    }
+    $("button").on("click",function () {
+      let type = $(this).html();
+      let email = $(this).attr("value");
+
+      if (type == 'Disable') {
+        for (let i = 1; i < persons.length; i++) {
+          if(persons[i]['email'] == email){
+            persons[i]['active'] = false;
+            break;
+          }
+        }
+      }
+      
+      if (type == 'Enable') {
+        for (let i = 1; i < persons.length; i++) {
+          if(persons[i]['email'] == email){
+            persons[i]['active'] = true;
+            break;
+          }
+        } 
+      }
+      
+      if (type == 'Delete') {
+        for (let i = 1; i < persons.length; i++) {
+          if(persons[i]['email'] == email){
+            persons = persons.filter(function(value, index, arr){ 
+                return value['email'] != email;
+            });
+            break;
+          }
+        } 
+      }
+
+    window.localStorage.setItem('users', JSON.stringify(persons));
+    location.reload();
+
+    });
+  });
+  
+}
